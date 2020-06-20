3b0b67c552641bed3a82e4adb986193c15dec0e1
shiva80001 <shiva80001@gmail.com>
2020-06-20 14:41:19 +0530

    new line added

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -7,6 +7,8 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("HelloWOrld");
+System.out.println("my World");
+System.out.println("Welcome");
 	}
 
 }
