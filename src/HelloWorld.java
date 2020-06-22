810fcd6dacc94fab694ab6612bd1be548ce69ffa
shiva80001 <shiva80001@gmail.com>
2020-06-22 11:20:41 +0530

    changes
    changes

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -21,7 +21,8 @@ System.out.println("u can do want you want");
 System.out.println("win");		
 System.out.println("problem solved");		
 System.out.println("no commit");		
-System.out.println("some changes");		
+System.out.println("some changes");	
+System.out.println("changes);		
 	}
 
 }
