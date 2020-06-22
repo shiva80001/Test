b51971c98f9248d4fa60f9d11b5c20463c8cc6c8
shiva80001 <shiva80001@gmail.com>
2020-06-22 11:26:17 +0530

    removed white space
    removed white space

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -11,44 +11,18 @@ System.out.println("HelloWOrld");
 System.out.println("do");
 System.out.println("my World");
 System.out.println("Welcome");
-<<<<<<< HEAD
-
-=======
 System.out.println("Welcome to india");
 System.out.println("Welcome to My World");		
-<<<<<<< HEAD
->>>>>>> 8f52fb302514f340557c232f9104c38aa80ca872
-
-=======
 System.out.println("how are you");
-<<<<<<< HEAD
-<<<<<<< HEAD
->>>>>>> f4b594fff7a1c678298b07f6d68f3b05474c403d
-=======
 System.out.println("what u want");		
->>>>>>> 123c097483c3b33f992cceb6b3c6de5cde6afe7a
-=======
 System.out.println("what u want");	
 System.out.println("i can achieve my dreams");		
-<<<<<<< HEAD
->>>>>>> 61e05b822b3a3f6603d623ce45fd9088ed87ce61
-=======
 System.out.println("i will buy Avenger Bike");		
-<<<<<<< HEAD
->>>>>>> 7499d39ea2a4290e0ccd714769c7a27b9821679a
-=======
 System.out.println("yes");		
 System.out.println("u can do want you want");		
 System.out.println("win");		
-<<<<<<< HEAD
->>>>>>> 5083a24d0dda50b8f128a725b79f9e3c29df2de4
-=======
 System.out.println("problem solved");		
-<<<<<<< HEAD
->>>>>>> 7fbc2cbeb14863c3a64600956ffcce508ca72b91
-=======
 System.out.println("no commit");		
->>>>>>> d3942798f8b24cc10dc2cd6bd3df68099a4d5b4c
 	}
 
 }
